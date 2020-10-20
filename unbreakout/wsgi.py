1385f4514274fc9a089cd31b0e9da526160e57bc
Dirk Uys <dirkcuys@gmail.com>
2020-10-20 21:39:46 +0200

    Rename project

--- unbreakout/wsgi.py
+++ unbreakout/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for zoomapp project.
+WSGI config for unbreakout project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zoomapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unbreakout.settings')
 
 application = get_wsgi_application()
