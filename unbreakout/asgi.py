8dd3974ee85e7321f82e1025bd1d62deb6fab3e1
Dirk Uys <dirkcuys@gmail.com>
2020-10-26 18:38:43 +0200

    Fix copypasta

--- unbreakout/asgi.py
+++ unbreakout/asgi.py
@@ -10,6 +10,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reunhangout.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unbreakout.settings")
 django.setup()
 application = get_default_application()
