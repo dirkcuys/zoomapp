88bdfd77054ce1831b616f3ef9d15d928bb1285c
Dirk Uys <dirkcuys@gmail.com>
2020-10-28 15:07:04 +0200

    Fix URL

--- meetings/urls.py
+++ meetings/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list', views.list_meetings, name='list'),
-    re_path(r'^(?P<short_code>\w+)$', views.registration, name='registration'),
     re_path(r'^m/(?P<slug>\w+)$', views.unbreakout, name='meeting'),
 
     path('create', views.create, name='create'),
@@ -15,4 +14,6 @@ urlpatterns = [
     re_path(r'^(?P<slug>\w+)/export$', views.export_breakouts, name='export'),
     re_path(r'^(?P<slug>\w+)/create_breakout$', views.create_breakout, name='create_breakout'),
     path('<slug:slug>/breakout/<int:breakout_id>/join', views.join_breakout, name='join_breakout'),
+
+    re_path(r'^(?P<short_code>\w+)$', views.registration, name='registration'),
 ]
